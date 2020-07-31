f1a77770275d4dc86a1865b3d541770461d9ccac
Vaibhav Srivastava <vaibhav.sri1195@gmail.com>
2020-07-31 17:54:02 +0530

    Create CVehicleDataLoggerTest.cpp

---
+++ CVehicleDataLoggerTest.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+ 
+TEST(COLLECT, Should) {
+    GetVehicleParameter(const TMetaData& metaData)
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
